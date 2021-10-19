2bcd12d34a8df1e0adaabdfa5f9b2c2bca28f8ec
Arian <arian.kulmer@web.de>
2021-10-19 19:28:10 +0200

    overlay: Convert Android.mk files to Android.bp

--- overlay/Android.mk
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2020-2021 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-LOCAL_PATH := $(call my-dir)
-
-include $(call first-makefiles-under,$(LOCAL_PATH))
