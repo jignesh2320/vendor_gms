4bb9b140add1cc98f05bbee0f34f15cba854e98d
Arian <arian.kulmer@web.de>
2021-10-19 19:24:32 +0200

    Drop java includes in FontGoogleSansOverlay

--- overlay/FontGoogleSansOverlay/Android.mk
+++ overlay/FontGoogleSansOverlay/Android.mk
@@ -10,7 +10,6 @@ include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := FontGoogleSansOverlay
 LOCAL_RRO_THEME := FontGoogleSans
 LOCAL_PRODUCT_MODULE := true
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_SDK_VERSION := current
 include $(BUILD_RRO_PACKAGE)
