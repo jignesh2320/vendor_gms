ccea59479624a53fdd6db8ad2a8eeb83e97960a2
Arian <arian.kulmer@web.de>
2021-10-19 17:38:45 +0200

    Append google-sans font instead of overwriting fonts_customization

--- products/Android.mk
+++ products/Android.mk
@@ -17,11 +17,11 @@ include $(BUILD_PREBUILT)
 # We can not use LOCAL_INSTALLED_MODULE_STEM := fonts_customization.xml because that will create overriding targets
 # This is pretty ugly but allow us to disable BUILD_BROKEN_DUP_RULES
 fonts_customization := $(TARGET_OUT_PRODUCT_ETC)/fonts_customization.xml
-fonts_customization_dummy := $(TARGET_OUT_PRODUCT_ETC)/fonts_customization.dummy.xml
+fonts_customization_merged := $(TARGET_OUT_PRODUCT_ETC)/fonts_customization.merged.xml
 fonts_customization_google-sans := $(TARGET_OUT_PRODUCT_ETC)/fonts_customization.google-sans.xml
 
-$(fonts_customization_dummy): $(fonts_customization) $(fonts_customization_google-sans)
-	rm $(fonts_customization)
-	mv $(fonts_customization_google-sans) $(fonts_customization)
+$(fonts_customization_merged): $(fonts_customization) $(fonts_customization_google-sans)
+	(head -n -1 fonts_customization.xml; echo ""; tail -n +3 fonts_customization.google-sans.xml) > fonts_customization.merged.xml
+	mv $(fonts_customization_merged) $(fonts_customization)
 
-ALL_DEFAULT_INSTALLED_MODULES += $(fonts_customization_dummy)
+ALL_DEFAULT_INSTALLED_MODULES += $(fonts_customization_merged)
